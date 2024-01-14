d3f1dba14930c99c35315a41ff7e5cd3534ece6d
PrabhuChandran-TS198 <prabhuc@tendersoftware.in>
2024-01-14 09:14:55 +0530

    Modified

--- test/desktop_active_window_test.dart
+++ test/desktop_active_window_test.dart
@@ -1,7 +1,6 @@
 import 'package:desktop_active_window/desktop_active_window.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:built_collection/built_collection.dart';
 
 
 
